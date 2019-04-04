70d75010e0565b0221bdcc78875f25f8af99bf33
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-04-04 14:11:28 +0200

    Adding a unit test for the welcome route

---
+++ tests/welcome.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+
+const server = require('../server/app')
+
+describe('GET /api/welcome', () => {
+  it('should return the welcome message', async () => {
+    const { body } = await request(server)
+      .get('/api/welcome')
+      .send()
+      .expect(200)
+
+    expect(body.message).toBe('Hello from express server!')
+  })
+})
